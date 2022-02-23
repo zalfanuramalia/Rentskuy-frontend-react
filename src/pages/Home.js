0913933e89e97529022769c945f67600f0ed8400
zalfanuramalia <zalfafanur@gmail.com>
2022-02-23 12:37:44 +0700

    chore: revision home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ import item2 from '../assets/images/telukbogam.png'
 import item3 from '../assets/images/bromo.png'
 import item4 from '../assets/images/malioboro.png'
 import element from '../assets/images/element.png'
-import elementTwo from '../assets/images/element-2.png'
+// import elementTwo from '../assets/images/element-2.png'
 import people from '../assets/images/people.png'
 import {FaStar} from 'react-icons/fa'
 
@@ -42,9 +42,9 @@ export default class Home extends Component {
               <h1 className="text-2"> Vehicle Finder </h1>
               <div><i className="fa-solid fa-horizontal-rule"></i></div>
             </div>
-            <form>
+            <form className='mt-5'>
               <div className="row type-1">
-                <div className="col select-1">
+                <div className="my-2 col-3 select-1 position-relative">
                   <select className="form-select" aria-label="Default select example">
                     <option selected>Location</option>
                     <option value="1">Yogyakarta</option>
@@ -53,7 +53,7 @@ export default class Home extends Component {
                   </select>
                   <i class="fa-solid fa-chevron-down"></i>
                 </div>
-                <div className="col select-2">
+                <div className="my-2 col-3 select-2 position-relative">
                   <select className="form-select" aria-label="Default select example">
                     <option selected>Type</option>
                     <option value="1">Cars</option>
@@ -64,7 +64,7 @@ export default class Home extends Component {
                 </div>
               </div>
               <div className="row type-2">
-                <div className="col select-1">
+                <div className="my-2 col-3 select-1 position-relative">
                   <select className="form-select" aria-label="Default select example">
                     <option selected>Payment</option>
                     <option value="1">One</option>
@@ -73,7 +73,7 @@ export default class Home extends Component {
                   </select>
                   <i class="fa-solid fa-chevron-down"></i>
                 </div>
-                <div className="col select-2">
+                <div className="my-2 col-3 select-2 position-relative">
                   <select className="form-select" aria-label="Default select example" id="datepicker">
                     <option selected>Date</option>
                   </select>
@@ -86,28 +86,24 @@ export default class Home extends Component {
         </header>
         <footer>
           <div className="texts d-flex justify-content-between">
-            <div>
-              <p className="hei">Popular in Town </p>
-            </div>
-            <div>
-              <p className="hai">view all <i class="fa-solid fa-chevron-right"> </i></p> 
-            </div>      
+            <p className="hei">Popular in Town </p>
+            <p className="hai">view all <i class="fa-solid fa-chevron-right"> </i></p> 
           </div>
           <div className='image container'>
             <div className="row">
-              <div className="col-6 col-lg-3 ">
+              <div className="col-6 col-md-3 ">
                 <img src={item1} alt="Merapi" />
                 <div className="merapi">Merapi Yogyakarta  </div>
               </div>
-              <div className="col-6 col-lg-3 ">
+              <div className="col-6 col-md-3 ">
                 <img src={item2} alt="TelukBogam" />
                 <div className="telukbogam">Teluk Bogam Kalimantan  </div>
               </div>
-              <div className="col-6 col-lg-3 ">
+              <div className="col-6 col-md-3 ">
                 <img src={item3} alt="Bromo" />
                 <div className="bromo">Bromo Malang </div>
               </div>
-              <div className="col-6 col-lg-3 ">
+              <div className="col-6 col-md-3 ">
                 <img src={item4} alt="Malioboro" />
                 <div className="malioboro">Malioboro Yogyakarta  </div>
               </div>
@@ -115,9 +111,7 @@ export default class Home extends Component {
           </div>
           <div className="text-pic">
             <div className="d-inline-block position-relative img-1">
-              <img src={element} alt="Element" />
-              <img src={elementTwo} alt="Element-2" class="element" />
-              <div className="element-2"><i class="fa-solid fa-plus"></i></div>
+              <img className='element' src={element} alt="Element" />
               <div className="d-flex element-3">
                 <i class="fa-solid fa-circle-chevron-left"></i>
                 <i class="fa-solid fa-circle-chevron-right"></i>
@@ -128,8 +122,8 @@ export default class Home extends Component {
             <p>Testimonials</p>
           </div>
           <div className="row">
-            <div className="col">
-              <div className="stars d-flex">
+            <div className="col flex-row-reverse">
+              <div className="stars text-center text-md-start d-flex justify-content-justify">
                 <FaStar />
                 <FaStar />
                 <FaStar />
@@ -139,13 +133,13 @@ export default class Home extends Component {
               <div className="text-description d-flex">
                 <img src={element}alt="Element" />
                 <p className="d-text"> ”It was the right decision to rent vehicle here, I spent less money and enjoy the trip. It was an amazing experience to have a ride for wildlife trip!”</p>
-                </div>
+              </div>
               <div className="s-text">
                 <p className="s-text-1">Edward Newgate</p>
                 <p className="d-text-1">Founder Circle</p>
               </div>
             </div>
-            <div className="col">
+            <div className="col name">
               <img src={people} alt="People" />
             </div>
           </div>
