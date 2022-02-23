58a4fa539194f0460ea23c4400687ad802aeab81
zalfanuramalia <zalfafanur@gmail.com>
2022-02-23 20:20:40 +0700

    chore: revision className

--- src/components/NavbarHome.js
+++ src/components/NavbarHome.js
@@ -10,27 +10,27 @@ export default class NavbarHome extends Component {
             <a className="navbar-brand" href="/Home">
               <img src={logo} alt="logo" />
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav ms-auto me-5 mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="./home.html">Home</a>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav ms-auto me-5 mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="./home.html">Home</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="./vehicle-type.html">Vehicle Type</a>
+                <li className="nav-item">
+                  <a className="nav-link" href="./vehicle-type.html">Vehicle Type</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="./history.html">History</a>
+                <li className="nav-item">
+                  <a className="nav-link" href="./history.html">History</a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="/Home">About</a>
+                <li className="nav-item">
+                  <a className="nav-link" href="/Home">About</a>
                 </li>
               </ul>
-              <div class="toggled-action">
-                <button class="button-outline mx-3" > <a href="./login.html">Login</a></button>
-                <button class="button-filled" onClick={this.onRegister} > Register</button>
+              <div className="toggled-action">
+                <button className="button-outline mx-3" > <a href="./login.html">Login</a></button>
+                <button className="button-filled" onClick={this.onRegister} > Register</button>
               </div>
             </div>
           </div>
