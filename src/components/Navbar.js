4597d7c25724d19d22d3e23d9a732cab5b2ab5f4
zalfanuramalia <zalfafanur@gmail.com>
2022-02-27 10:43:58 +0700

    chore: add link to logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,9 +10,9 @@ export default class Navbar extends Component {
     return (
       <nav className="navbar navbar-expand-lg navbar-light">
       <div className="container">
-        <a className="navbar-brand" href="Home">
+        <Link to='/' className="navbar-brand">
           <img src={logo} alt="logo" className='logo'/>
-        </a>
+        </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
--- src/components/NavbarHome.js
+++ src/components/NavbarHome.js
@@ -7,9 +7,9 @@ export default class NavbarHome extends Component {
         return (
             <nav className="navbar navbar-expand-lg navbar-light">
           <div className="container">
-            <a className="navbar-brand" href="/Home">
-              <img src={logo} alt="logo" className='logo' color='black' />
-            </a>
+            <Link to='/' className="navbar-brand">
+              <img src={logo} alt="logo" className='logo'/>
+            </Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
