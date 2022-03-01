b1528753b67a435bcd5b3b6dda6d76aa6651c2e3
zalfanuramalia <zalfafanur@gmail.com>
2022-03-01 17:24:23 +0700

    chore: change to function component

--- src/pages/Reservation.js
+++ src/pages/Reservation.js
@@ -1,24 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
-import { FaChevronLeft, FaChevronDown } from 'react-icons/fa'
+import { FaChevronLeft } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
-export default class Reservation extends Component {
-    state = {
-        value: 0
-    }
+import { Button } from '../components/Button'
+import fixie from '../assets/images/fixie-grey.png'
 
-    changeValue = (tombol) => {
-        if(tombol === "minus"){
-            this.setState({value: this.state.value-1})
-        } else {
-            this.setState({value: this.state.value+1})
-        }
-        
-    }
-
-  render() {
-    return (
+export const Reservation = () => {
+      
+      return (
       <>
         <Navbar />
         <div className="payment">
@@ -28,18 +18,14 @@ export default class Reservation extends Component {
         <div className="container-fluid g-0">
           <div className="row g-0">
             <div className="col">
-              <img src="./assets/images/fixie-2.png" alt="Fixie" />
+              <img src={fixie} alt="Fixie" />
             </div>
             <div className="col info">
               <div>
                 <h1 className="text-1">Fixie - Gray Only </h1>
                 <h1 className="text-2"> Yogyakarta</h1>
                 <p className="text-3">No Prepayment</p>
-                <div className="qty ">
-                    <button className="minus " onClick={()=>this.changeValue("minus")}>-</button>
-                    <div type="number" className="count" name="qty">{this.state.value}</div>
-                    <button className="plus " onClick={()=>this.changeValue("plus")}>+</button>
-                </div>
+                <Button></Button>
                 <div className="text-4">
                   <p>Reservation Date :</p>
                 </div>
@@ -56,18 +42,95 @@ export default class Reservation extends Component {
                     <option value="2">3 Day</option>
                     <option value="3">4 Day</option>
                   </select>
-                  <FaChevronDown />
                 </div>
               </div>
             </div>
           </div>
         </div>
         <div className="pay-1">
-          <button>Pay now : Rp. 178.000</button>
+          <Link to='/Payment' className="nav-link active" aria-current="page"><button>Pay now : Rp. 178.000</button></Link>
+          
         </div>
       </div>
   <Footer />
       </>
     )
-  }
 }
+
+export default Reservation
+
+
+// import React, { Component } from 'react'
+// import Footer from '../components/Footer'
+// import Navbar from '../components/Navbar'
+// import { FaChevronLeft, FaChevronDown } from 'react-icons/fa'
+// import { Link } from 'react-router-dom'
+
+// export default class Reservation extends Component {
+//     state = {
+//         value: 0
+//     }
+
+//     changeValue = (tombol) => {
+//         if(tombol === "minus"){
+//             this.setState({value: this.state.value-1})
+//         } else {
+//             this.setState({value: this.state.value+1})
+//         }
+        
+//     }
+
+//   render() {
+//     return (
+//       <>
+//         <Navbar />
+//         <div className="payment">
+//         <div className="reserve">
+//           <Link to='/VehicleType' style={{textDecoration:"none"}} className="d-flex"> <span><FaChevronLeft /></span>  <p>Reservation</p> </Link>
+//         </div>
+//         <div className="container-fluid g-0">
+//           <div className="row g-0">
+//             <div className="col">
+//               <img src="./assets/images/fixie-2.png" alt="Fixie" />
+//             </div>
+//             <div className="col info">
+//               <div>
+//                 <h1 className="text-1">Fixie - Gray Only </h1>
+//                 <h1 className="text-2"> Yogyakarta</h1>
+//                 <p className="text-3">No Prepayment</p>
+//                 <div className="qty ">
+//                     <button className="minus " onClick={()=>this.changeValue("minus")}>-</button>
+//                     <div type="number" className="count" name="qty">{this.state.value}</div>
+//                     <button className="plus " onClick={()=>this.changeValue("plus")}>+</button>
+//                 </div>
+//                 <div className="text-4">
+//                   <p>Reservation Date :</p>
+//                 </div>
+//                 <div className="op-1">
+//                   <div className="date" id="datepicker">
+//                     <input type="text" class="form-control pick" placeholder="Select date" />
+//                     <span className="input-group-append"></span>
+//                   </div>       
+//                 </div>
+//                 <div className="op-2">
+//                   <select className="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+//                     <option selected>1 Day</option>
+//                     <option value="1">2 Day</option>
+//                     <option value="2">3 Day</option>
+//                     <option value="3">4 Day</option>
+//                   </select>
+//                   <FaChevronDown />
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="pay-1">
+//           <button>Pay now : Rp. 178.000</button>
+//         </div>
+//       </div>
+//   <Footer />
+//       </>
+//     )
+//   }
+// }
