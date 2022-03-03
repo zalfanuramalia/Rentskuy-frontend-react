c3a398e29dd12baf5774061b336de1964f006fd3
zalfanuramalia <zalfafanur@gmail.com>
2022-03-03 17:24:10 +0700

    chore: add promise

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
 import logger from 'redux-logger'
-// import promise from 'redux-promise-middleware'
+import promise from 'redux-promise-middleware'
 
 const store = createStore(
     rootReducer,
     applyMiddleware(
-        
+        promise,
         logger
     )
 )
