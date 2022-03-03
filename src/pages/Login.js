b3d4afac5488e4ae823a70901eef1a761bf6a6d7
zalfanuramalia <zalfafanur@gmail.com>
2022-03-03 15:04:16 +0700

    chore: add auth for login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,92 +1,97 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
 import {FaTwitter, FaFacebookF, FaInstagram, FaLinkedinIn, FaYoutube} from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import google from '../assets/images/google.png'
 import logo from '../assets/images/logo.png'
+// import Input from '../components/Input'
+// import Button from '../components/Button'
+import { Navigate } from 'react-router-dom'
 
+export const Login = ({auth, dispatch, counter}) => {
 
-export default class Login extends Component {
-    state = {
-        email : '',
-        password: '',
-        isLogged: false
-    }
+  useEffect(()=>{
+    console.log(auth);
+  })
 
-    handleEmailChange =(e)=>{
-        this.setState({email:e.target.value})
-    }
-    handlePasswordChange=(e)=>{
-        this.setState({password:e.target.value})
-    }
-    
+  const onLogin = (e)=>{
+    e.preventDefault()
+    dispatch({
+      type: 'LOGIN',
+      payload: {
+        email: e.target.elements['email'].value,
+        password: e.target.elements['password'].value
+      }
+    })
+  }
 
-    onLogin = (e)=>{
-        e.preventDefault()
-        if(this.state.email === "admin@mail.com" && this.state.password==="1234"){
-        this.setState({isLogged: true})
-        this.props.onLogin(true)
-        }else{
-        window.alert("Email atau password salah!")
-        this.props.onLogin(false)
-        }
-    }
+//   const onLogout = ()=>{
+//     dispatch({
+//       type: 'LOGOUT'
+//     })
+//   }
 
-  render() {
-    return (
-      <>
-        <div className="container-fluid g-0 login">
-            <div className="row g-0">
-                <div className="col-6 bg-image"></div>
-                <div className="col text2">
-                    <form className="overflow-auto">
-                        <h1 className="text-login">Login</h1>
-                        <div>
-                            <div className="input-group-md">
-                                <input onChange={this.handleEmailChange} className='mb-5 form-control' type='email' placeholder='Email'  />
-                            </div>
-                            <div className="input-group-md">
-                                <input onChange={this.handlePasswordChange} className='mb-5 form-control' type='password' placeholder='Password'  />
-                            </div>
-                            <div className="mb-3">
-                                <button onClick={this.onLogin} className='button-send btn btn-primary' >Login</button>
-                            </div>
-                            <div className="mb-3">
-                                <Link to='/ForgotPassword' className="nav-link active" aria-current="page">Forgot Password?</Link>
-                            </div>
-                            <div className="d-flex position-relative justify-content-center align-items-center flex-column my-5 text-again">
-                                <div className="border border-bottom-1 w-100"></div>
-                                <div className="position-absolute px-3 text-try">or try another way</div>
-                            </div>
-                            <div>
-                                <button className="button-mail"> <img src={google} alt="Logo" /><span>    Login with Google     </span> </button> 
-                            </div>
-                            <div>
-                                <button className="button-register"><a href="/register.html">Sign Up</a></button>
-                            </div>
+  return (
+    <>
+    {auth.token!==null && <Navigate to='/homepage' />}
+    <div className="container-fluid g-0 login">
+        <div className="row g-0">
+            <div className="col-6 bg-image"></div>
+            <div className="col text2">
+                <form onSubmit={onLogin} className="overflow-auto">
+                    <h1 className="text-login">Login</h1>
+                    <div>
+                        <div className="input-group-md">
+                            <input  className='mb-5 form-control' type='email' id='email' placeholder='Email'  />
                         </div>
-                    </form>
-                    <div className="text-desc">
-                        <div className="row g-0 px-5 text-desc-1">
-                            <div className="col-12">
-                                <div>
-                                    <img src={logo} alt="Logo" />
-                                </div>
-                                <div className="description">Plan and book your perfect trip with expert advice, travel tips for vehicle information from us</div>
-                                <div className="desc">©2020 Vehicle Rental Center. All rights reserved</div>
+                        <div className="input-group-md">
+                            <input  className='mb-5 form-control' type='password' id='password' placeholder='Password'  />
+                        </div>
+                        <div className="mb-3">
+                            <button type='submit' className='button-send btn btn-primary' >Login</button>
+                        </div>
+                        <div className="mb-3">
+                            <Link to='/ForgotPassword' className="nav-link active" aria-current="page">Forgot Password?</Link>
+                        </div>
+                        <div className="d-flex position-relative justify-content-center align-items-center flex-column my-5 text-again">
+                            <div className="border border-bottom-1 w-100"></div>
+                            <div className="position-absolute px-3 text-try">or try another way</div>
+                        </div>
+                        <div>
+                            <button className="button-mail"> <img src={google} alt="Logo" /><span>    Login with Google     </span> </button> 
+                        </div>
+                        <div>
+                            <button className="button-register"><a href="/register.html">Sign Up</a></button>
+                        </div>
+                    </div>
+                </form>
+                <div className="text-desc">
+                    <div className="row g-0 px-5 text-desc-1">
+                        <div className="col-12">
+                            <div>
+                                <img src={logo} alt="Logo" />
                             </div>
-                            <div className="link-social mt-5">
-                                <FaTwitter />
-                                <FaFacebookF />
-                                <FaInstagram />
-                                <FaLinkedinIn />
-                                <FaYoutube />
-                            </div>
+                            <div className="description">Plan and book your perfect trip with expert advice, travel tips for vehicle information from us</div>
+                            <div className="desc">©2020 Vehicle Rental Center. All rights reserved</div>
+                        </div>
+                        <div className="link-social mt-5">
+                            <FaTwitter />
+                            <FaFacebookF />
+                            <FaInstagram />
+                            <FaLinkedinIn />
+                            <FaYoutube />
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-      </>
-    )
-  }
+    </div>
+  </>
+  )
 }
+
+const mapStateToProps = state => ({auth:state.auth, counter: state.counter})
+
+const mapDispatchToProps = dispatch => ({dispatch})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login)
