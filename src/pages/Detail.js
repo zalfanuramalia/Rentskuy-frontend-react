0b3db0586ef52876491efc33ace30b1befc0a27f
zalfanuramalia <zalfafanur@gmail.com>
2022-03-06 18:06:57 +0700

    chore: get data from redux

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,48 +1,50 @@
 import React, { useEffect, useState } from 'react'
 import { useParams, useNavigate, Link } from 'react-router-dom'
 import Navbar from '../components/Navbar'
-import { getData } from '../helpers/http'
 import Footer from '../components/Footer'
+import { increment, decrement } from '../redux/actions/buttons'
 import { FaHeart, FaChevronLeft, FaChevronRight } from 'react-icons/fa'
-import Button from '../components/Button'
+import { connect, useDispatch, useSelector } from 'react-redux'
+import NumberFormat from 'react-number-format'
+import { getDetail } from '../redux/actions/detail'
 
-export const Detail = (props) => {
+export const Detail = ({getDetail}) => {
+    const {detail} = useSelector(state => state)
     const [vehicles, setVehicles] = useState({})
-    const [count, setCount] = useState(0)    
+    const buttons = useSelector(state=>state.buttons)
 
-    const onPlus = (count) => {
-      setCount([count + 1]);
-      
-    };
-    const onMinus = (count) => {
-      setCount(count - 1);
-    };
-
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
     const {id} = useParams()
 
     useEffect(()=>{
-        getDataComponent(id)
+      console.log(buttons);
+    }, [buttons])
+
+    useEffect(()=>{
+        getDetail(id)
     },[])
 
     useEffect(()=>{
       console.log(vehicles)
     },[])
 
-    // useEffect(()=>{
-    //   setCount(count)
-    // },[])
-
-    const getDataComponent = async (id)=>{
-        try{
-            const {data} = await getData(`http://localhost:8080/vehicles/${id}`, props.history)
-            setVehicles(data.results)
-        }catch(e){
-          console.log(e)
-        }
+    const onIncrement = (e)=>{
+      e.preventDefault()
+      dispatch(increment())
+    }
+  
+    const onDecrement = (e)=>{
+      e.preventDefault()
+      dispatch(decrement())
     }
 
     const goBack = () => {
       window.history.back()
+    }
+
+    const goReservation = (id)=> {
+      navigate(`/reservation/${id}`)
     }
 
   return (
@@ -54,24 +56,26 @@ export const Detail = (props) => {
         <p>Detail</p> 
       </div>
       <div className="vehicles">
-        <div className="vehicles-details d-flex flex-wrap justify-content-start position-relative">
+        <div className="vehicles-details d-flex flex-wrap position-relative">
           <div className="main-image">
-            <img className='img-fluid' src={vehicles?.image} alt="charImg" />
+            <img className='img-fluid' src={detail.detail?.image} alt="charImg" />
           </div>  
-          <div className="description">
+          <div className="description my-5">
             <div className="details p-1 position-relative">
               <div className="city">
-                <h2>{vehicles?.brand}</h2>
-                <h3>{vehicles?.location}</h3>
+                <h2>{detail.detail?.brand}</h2>
+                <h3>{detail.detail?.location}</h3>
               </div>
-              <h4 className='available'>{vehicles?.isAvailable}</h4>
-              <p className='pre'>{vehicles?.can_prepayment}</p>
+              <h4 className='available'>{detail.detail?.isAvailable}</h4>
+              <p className='pre'>{detail.detail?.can_prepayment}</p>
               <div class="about">
-                <p>Capacity : {vehicles?.qty}</p>
-                <p>Type : {vehicles?.category_id}</p>
+                <p>Capacity : {detail.detail?.qty}</p>
+                <p>Type : {detail.detail?.category_id}</p>
                 <p>Reservation before 2 PM</p>
               </div>
-              <div className="price position-absolute">Rp.{vehicles?.price}/day</div>
+              <div className="price position-absolute">
+                <NumberFormat value={detail.detail?.price} displayType={'text'} thousandSeparator={'.'} decimalSeparator={','} prefix={'Rp. '} suffix={'/day'} ></NumberFormat>
+              </div>
             </div>
           </div>
         </div>
@@ -82,13 +86,17 @@ export const Detail = (props) => {
             <div className='left'>
               <FaChevronLeft />
             </div>             
-            <img className='img-fluid' src={vehicles?.image} alt='charImg' />
-            <img className='img-fluid' src={vehicles?.image} alt='charImg' />
+            <img className='img-fluid' src={detail.detail?.image} alt='charImg' />
+            <img className='img-fluid' src={detail.detail?.image} alt='charImg' />
             <div className='right'>
               <FaChevronRight />
-            </div>            
+            </div>         
+            <div className='d-flex justify-content-center align-items-center counting'>         
+            <button className="minus " onClick={onDecrement}>-</button>
+            <h1 className="count">{buttons.value}</h1>
+            <button className="plus "  onClick={onIncrement}>+</button>
+          </div>   
           </div>
-          <Button></Button>
         </div>
       </form>
       <div className="button container">
@@ -96,9 +104,7 @@ export const Detail = (props) => {
           <div className='button-chat btn btn-primary col-2'>
             <Link to='/' style={{textDecoration:"none"}} type="button"> <p>Chat Admin</p></Link>
           </div>
-          <div className="submit-button btn btn-primary">
-            <Link to='/reservation' style={{textDecoration:"none"}} > <p>Reservation</p> </Link>
-          </div>
+          <button onClick={()=>goReservation(id)} style={{cursor: 'pointer'}} className="submit-button btn btn-primary"> <p>Reservation</p></button>
           <div className="button-like btn btn-primary ">
             <Link to="/" style={{textDecoration:"none"}} className='d-flex justifiy-content-around' role="button"> <span><FaHeart /> </span> <p>Like</p> </Link>
           </div>
@@ -110,4 +116,8 @@ export const Detail = (props) => {
   )
 }
 
-export default Detail
+const mapStateToProps = state => ({detail: state.detail})
+
+const mapDispatchToProps = {getDetail}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Detail)
