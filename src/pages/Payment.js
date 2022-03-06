445ee6639af5246ca042400cc63842a97f504619
zalfanuramalia <zalfafanur@gmail.com>
2022-03-06 18:05:42 +0700

    chore: get data from redux

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -1,29 +1,65 @@
-import React, { Component } from 'react'
-import Footer from '../components/Footer'
+import React, { useEffect, useState } from 'react'
 import Layout from '../components/Layout'
-import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import { FaChevronLeft } from 'react-icons/fa'
+import { https } from '../helpers/https'
+import { useParams } from 'react-router-dom'
+import { connect, useDispatch, useSelector } from 'react-redux'
+import { getDetail } from '../redux/actions/detail'
 
+export const Payment = ({getDetail}) => {
+  const {detail} = useSelector(state => state)
+  const [vehicles, setVehicles] = useState({})
+  const buttons = useSelector(state=>state.buttons)
+  const [text, setText] = useState('');
 
-export default class Payment extends Component {
-  render() {
+  const dispatch = useDispatch()
+  const {id} = useParams()
+
+  const inputHandler = event => {
+    setText(event.target.value);
+  }
+
+  const copy = async () => {
+    await navigator.clipboard.writeText(text);
+    alert('Text copied');
+  }
+
+  useEffect(()=>{
+    console.log(buttons);
+  }, [buttons])
+
+  useEffect(()=>{
+    getDetail(id)
+  },[])
+
+  useEffect(()=>{
+    console.log(vehicles)
+  },[])
+
+  const goBack = () => {
+    window.history.back()
+  }
     return (
       <Layout>
         <div className="payment">
-    <div className="reserve">
-      <i className="fa-solid fa-chevron-left"></i>
-      <span>Payment</span>
+    <     div className="reserve">
+            <div onClick={goBack} className="back-btn d-flex justify-content-start back" style={{cursor: 'pointer'}}>
+              <span><FaChevronLeft /></span> 
+              <p>Payment</p> 
+            </div>
     </div>
     <div className="container-fluid g-0">
       <div className="row g-0 in">
         <div className="col info-img g-0">
-          <img src="./assets/images/fixie-3.png" alt="Fixie" />
+          <img src={detail.detail?.image} alt="Fixie" />
         </div>
         <div className="col info-pay g-0">
-          <h1 className="text-1">Fixie - Gray Only </h1>
-          <h1 className="text-2"> Yogyakarta</h1>
-          <p className="text-3">No Prepayment</p>
-          <p className="text-4">#FG1209878YZS</p>
-          <button className="text-5">Copy booking code</button>
+          <h1 className="text-1">{detail.detail?.brand} </h1>
+          <h1 className="text-2"> {detail.detail?.location}</h1>
+          <p className="text-3">{detail.detail?.can_prepayment}</p>
+          <p className="text-4" onChange={inputHandler}>#FG1209878YZS</p>
+          <button onClick={copy} className="text-5">Copy booking code</button>
         </div>
       </div>
     </div>
@@ -31,11 +67,11 @@ export default class Payment extends Component {
   <div className="result-1">
     <div className="d-flex res">
       <div className="res-1">
-        <p>Quantity : 2 bikes</p>
+        <p>Quantity : {buttons.value} cars</p>
       </div>
       <div className="d-inline-flex p-2 bd-highlight res-2">
         <p>Reservation Date :</p>
-        <span>Jan 18 - 20 2021</span>
+        <span>Jan 18 - 20, 2021</span>
       </div>
     </div>
     <div className="d-flex res-4">
@@ -55,14 +91,13 @@ export default class Payment extends Component {
       </div>
       <div className="d-flex ress-2">
         <p>#FG1209878YZS</p>
-        <button>Copy</button>
+        <button onClick={copy}>Copy</button>
       </div>
       <div className=" d-flex ress-3">
         <select className="form-select sel-op" aria-label="Default select example">
-          <option selected>Select payment methods</option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
+          <option style={{display:'none'}} selected>Select payment methods</option>
+          <option value="1">Cash</option>
+          <option value="2">Transfer</option>
         </select>
         <span><i className="fa-solid fa-chevron-down"></i></span>
       </div>
@@ -77,5 +112,10 @@ export default class Payment extends Component {
   <Footer />
       </Layout>
     )
-  }
 }
+
+const mapStateToProps = state => ({detail: state.detail})
+
+const mapDispatchToProps = {getDetail}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Payment)
--- src/pages/Reservation.js
+++ src/pages/Reservation.js
@@ -1,37 +1,82 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Footer from '../components/Footer'
 import { FaChevronLeft } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
-import { Button } from '../components/Button'
-import fixie from '../assets/images/fixie-grey.png'
+import { useParams, useNavigate } from 'react-router-dom'
+import { increment, decrement } from '../redux/actions/buttons'
+import { connect, useDispatch, useSelector } from 'react-redux'
 import Layout from '../components/Layout'
+import NumberFormat from 'react-number-format'
+import { getDetail } from '../redux/actions/detail'
 
-export const Reservation = () => {
+export const Reservation = ({getDetail}) => {
+      const {detail} = useSelector(state => state)
+      const [vehicles, setVehicles] = useState({})
+      const buttons = useSelector(state=>state.buttons)
+
+      const dispatch = useDispatch()
+      const navigate = useNavigate()
+      const {id} = useParams()
+
+      useEffect(()=>{
+        console.log(buttons);
+      }, [buttons])
+
+      useEffect(()=>{
+        getDetail(id)
+      },[])
+
+    useEffect(()=>{
+      console.log(vehicles)
+    },[])
+
+      const onIncrement = (e)=>{
+        e.preventDefault()
+        dispatch(increment())
+      }
+    
+      const onDecrement = (e)=>{
+        e.preventDefault()
+        dispatch(decrement())
+      }
+
+      const goBack = () => {
+        window.history.back()
+      }
+
+      const goPayment = (id)=> {
+        navigate(`/payment/${id}`)
+      }
+
       return (
       <Layout>
         <div className="payment">
         <div className="reserve">
-          <Link to='/VehicleType' style={{textDecoration:"none"}} className="d-flex"> <span><FaChevronLeft /></span>  <p>Reservation</p> </Link>
+          <div onClick={goBack} className="back-btn d-flex justify-content-start back" style={{cursor: 'pointer'}}>
+            <span><FaChevronLeft /></span> 
+            <p>Reservation</p> 
+          </div>
         </div>
         <div className="container-fluid g-0">
           <div className="row g-0">
             <div className="col">
-              <img src={fixie} alt="Fixie" />
+              <img className='img-fluid' src={detail.detail?.image} alt="charImg" />
             </div>
             <div className="col info">
               <div>
-                <h1 className="text-1">Fixie - Gray Only </h1>
-                <h1 className="text-2"> Yogyakarta</h1>
-                <p className="text-3">No Prepayment</p>
-                <Button></Button>
+                <h1 className="text-1">{detail.detail?.brand} </h1>
+                <h1 className="text-2"> {detail.detail?.location}</h1>
+                <p className="text-3">{detail.detail?.can_prepayment}</p>
+                <div className='d-flex justify-content-center align-items-center counting'>         
+                  <button className="minus " onClick={onDecrement}>-</button>
+                  <h1 className="count">{buttons.value}</h1>
+                  <button className="plus "  onClick={onIncrement}>+</button>
+                </div>
                 <div className="text-4">
                   <p>Reservation Date :</p>
                 </div>
                 <div className="op-1">
-                  <div className="date" id="datepicker">
-                    <input type="text" class="form-control pick" placeholder="Select date" />
-                    <span className="input-group-append"></span>
-                  </div>       
+                  <input className="form-control date" type="date" name="date" placeholder='Date' />
+                  <span className="input-group-append"></span>  
                 </div>
                 <div className="op-2">
                   <select className="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
@@ -46,7 +91,9 @@ export const Reservation = () => {
           </div>
         </div>
         <div className="pay-1">
-          <Link to='/Payment' className="nav-link active" aria-current="page"><button>Pay now : Rp. 178.000</button></Link>
+          <button onClick={()=>goPayment(id)} style={{cursor: 'pointer'}} className="submit-button nav-link active">
+            <NumberFormat value={detail.detail?.price * buttons.value} displayType={'text'} thousandSeparator={'.'} decimalSeparator={','} prefix={'Pay now : Rp. '}  >Rp.{vehicles?.price}/day</NumberFormat>
+          </button>
         </div>
       </div>
       <Footer />
@@ -54,80 +101,9 @@ export const Reservation = () => {
     )
 }
 
-export default Reservation
 
+const mapStateToProps = state => ({detail: state.detail})
 
-// import React, { Component } from 'react'
-// import Footer from '../components/Footer'
-// import Navbar from '../components/Navbar'
-// import { FaChevronLeft, FaChevronDown } from 'react-icons/fa'
-// import { Link } from 'react-router-dom'
+const mapDispatchToProps = {getDetail}
 
-// export default class Reservation extends Component {
-//     state = {
-//         value: 0
-//     }
-
-//     changeValue = (tombol) => {
-//         if(tombol === "minus"){
-//             this.setState({value: this.state.value-1})
-//         } else {
-//             this.setState({value: this.state.value+1})
-//         }
-        
-//     }
-
-//   render() {
-//     return (
-//       <>
-//         <Navbar />
-//         <div className="payment">
-//         <div className="reserve">
-//           <Link to='/VehicleType' style={{textDecoration:"none"}} className="d-flex"> <span><FaChevronLeft /></span>  <p>Reservation</p> </Link>
-//         </div>
-//         <div className="container-fluid g-0">
-//           <div className="row g-0">
-//             <div className="col">
-//               <img src="./assets/images/fixie-2.png" alt="Fixie" />
-//             </div>
-//             <div className="col info">
-//               <div>
-//                 <h1 className="text-1">Fixie - Gray Only </h1>
-//                 <h1 className="text-2"> Yogyakarta</h1>
-//                 <p className="text-3">No Prepayment</p>
-//                 <div className="qty ">
-//                     <button className="minus " onClick={()=>this.changeValue("minus")}>-</button>
-//                     <div type="number" className="count" name="qty">{this.state.value}</div>
-//                     <button className="plus " onClick={()=>this.changeValue("plus")}>+</button>
-//                 </div>
-//                 <div className="text-4">
-//                   <p>Reservation Date :</p>
-//                 </div>
-//                 <div className="op-1">
-//                   <div className="date" id="datepicker">
-//                     <input type="text" class="form-control pick" placeholder="Select date" />
-//                     <span className="input-group-append"></span>
-//                   </div>       
-//                 </div>
-//                 <div className="op-2">
-//                   <select className="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-//                     <option selected>1 Day</option>
-//                     <option value="1">2 Day</option>
-//                     <option value="2">3 Day</option>
-//                     <option value="3">4 Day</option>
-//                   </select>
-//                   <FaChevronDown />
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//         <div className="pay-1">
-//           <button>Pay now : Rp. 178.000</button>
-//         </div>
-//       </div>
-//   <Footer />
-//       </>
-//     )
-//   }
-// }
+export default connect(mapStateToProps, mapDispatchToProps)(Reservation)
