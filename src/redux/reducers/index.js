396e734512f1d91f4c6e30f3ee111e9bbcf30ac6
zalfanuramalia <zalfafanur@gmail.com>
2022-03-06 21:41:03 +0700

    chore: add error message

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -10,7 +10,7 @@ const dataState = {
     bike: [],
     pageInfo: {},
     isLoading: false,
-    error: false
+    isError: false,
 }
 
 
@@ -18,72 +18,84 @@ const rootReducer = combineReducers ({
     auth,
     buttons,
     detail,
+    car: (state=dataState, action)=> {
+      switch(action.type){
+          case 'GET_CAR_PENDING': {
+              state.isLoading =true
+              state.isError = false
+              return {...state}
+          }
+          case 'GET_CAR_FULFILLED': {
+              const {data} = action.payload
+              state.car = data.results
+              state.pageInfo = data.info
+              state.isLoading = false
+              state.isError = false
+              return {...state}
+          }
+          case 'GET_CAR_REJECTED': {
+              const {message} = action.payload.response.data
+              state.isLoading = false
+              state.isError = true
+              state.errorMsg = message
+              return {...state}
+          }
+          default: {
+              return {...state}
+          }
+      }
+    },
     popular: (state=dataState, action)=> {
       switch(action.type){
           case 'GET_POPULAR_PENDING': {
-              state.isLoading =true
-              return state
+              state.isLoading = true
+              state.isError = false
+              return {...state}
           }
           case 'GET_POPULAR_FULFILLED': {
               const {data} = action.payload
               state.popular = data.result
               state.pageInfo = data.info
               state.isLoading = false
-              return state
+              state.isError = false
+              return {...state}
           }
           case 'GET_POPULAR_REJECTED': {
+              const {message} = action.payload.response.data
               state.isLoading = false
               state.isError = true
-              return state
+              state.errorMsg = message
+              return {...state}
           }
           default: {
-              return state
+              return {...state}
           }
       }
-    },
-    car: (state=dataState, action)=> {
-        switch(action.type){
-            case 'GET_CAR_PENDING': {
-                state.isLoading =true
-                return state
-            }
-            case 'GET_CAR_FULFILLED': {
-                const {data} = action.payload
-                state.car = data.results
-                state.pageInfo = data.info
-                state.isLoading = false
-                return state
-            }
-            case 'GET_CAR_REJECTED': {
-                state.isLoading = false
-                state.isError = true
-                return state
-            }
-            default: {
-                return state
-            }
-        }
     },
     motorbike: (state=dataState, action)=> {
         switch(action.type){
             case 'GET_MOTORBIKE_PENDING': {
                 state.isLoading =true
-                return state
+                state.isError = false
+                return {...state}
             }
             case 'GET_MOTORBIKE_FULFILLED': {
                 const {data} = action.payload
                 state.motorbike = data.results
                 state.pageInfo = data.info
                 state.isLoading = false
-                return state
+                state.isError = false
+                return {...state}
             }
             case 'GET_MOTORBIKE_REJECTED': {
+                const {message} = action.payload.response.data
                 state.isLoading = false
                 state.isError = true
-                return state
+                state.errorMsg = message
+                return {...state}
             }
             default: {
-                return state
+                return {...state}
             }
         }
     },
@@ -91,22 +103,26 @@ const rootReducer = combineReducers ({
         switch(action.type){
             case 'GET_BIKE_PENDING': {
                 state.isLoading =true
-                return state
+                state.isError = false
+                return {...state}
             }
             case 'GET_BIKE_FULFILLED': {
                 const {data} = action.payload
                 state.bike = data.results
                 state.pageInfo = data.info
                 state.isLoading = false
-                return state
+                state.isError = false
+                return {...state}
             }
             case 'GET_BIKE_REJECTED': {
+                const {message} = action.payload.response.data
                 state.isLoading = false
                 state.isError = true
-                return state
+                state.errorMsg = message
+                return {...state}
             }
             default: {
-                return state
+                return {...state}
             }
         }
     }
